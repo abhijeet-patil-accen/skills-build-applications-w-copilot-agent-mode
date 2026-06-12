01b2d35c4d02f74b8ac2bc5c5e18fd31318dbf70
abhijeet-patil-accen <abhijeet.b.a.patil@accenture.com>
2026-06-12 01:37:42 +0000

    first commit

---
+++ octofit-tracker/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <Container className="py-5">
+      <h1>OctoFit Tracker</h1>
+      <p>Modern multi-tier fitness tracking experience.</p>
+    </Container>
+  )
+}
+
+export default App
